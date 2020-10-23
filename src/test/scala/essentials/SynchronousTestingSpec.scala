81ea6f7923035bfbe5ca33a27c50ddcad09c2a87
Valentin Popov <VOPopov@luxoft.com>
2020-10-23 18:15:03 +0300

    Synchronous testing

---
+++ src/test/scala/essentials/SynchronousTestingSpec.scala
@@ -0,0 +1,63 @@
+package essentials
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{CallingThreadDispatcher, TestActorRef, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.concurrent.duration.Duration
+
+// Don't use TestKit in synchronous tests
+// All actions run in the same thread!
+class SynchronousTestingSpec extends AnyWordSpecLike with BeforeAndAfterAll {
+
+  implicit val system: ActorSystem = ActorSystem("SynchronousTestingSpec")
+
+  override def afterAll(): Unit = {
+    system.terminate()
+  }
+
+  import SynchronousTestingSpec._
+
+  "A counter" should {
+    "synchronously increase its counter" in {
+      val counter = TestActorRef[Counter](Props[Counter])
+      counter ! Inc // counter has ALREADY received the message
+
+      assert(counter.underlyingActor.count == 1)
+    }
+
+    "synchronously increase its counter at the call of the receive function" in {
+      val counter = TestActorRef[Counter](Props[Counter])
+      counter.receive(Inc)
+      /**
+       * Retrieve reference to the underlying actor, where the static type matches the factory used inside the
+       * constructor. Beware that this reference is discarded by the ActorRef upon restarting the actor (should this
+       * reference be linked to a supervisor). The old Actor may of course still be used in post-mortem assertions.
+       */
+      assert(counter.underlyingActor.count == 1)
+    }
+
+    "work on the calling thread dispatcher" in {
+      val counter = system.actorOf(Props[Counter].withDispatcher(CallingThreadDispatcher.Id))
+      val probe = TestProbe()
+
+      probe.send(counter, Read)
+      probe.expectMsg(Duration.Zero, 0) // probe has ALREADY received the message 0
+    }
+  }
+}
+
+object SynchronousTestingSpec {
+  case object Inc
+  case object Read
+
+  class Counter extends Actor {
+    var count = 0
+
+    override def receive: Receive = {
+      case Inc => count += 1
+      case Read => sender() ! count
+    }
+  }
+}
