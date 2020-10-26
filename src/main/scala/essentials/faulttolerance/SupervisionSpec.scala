4001bdde06d15e56fbf3da57f2c8a1a99bd925b4
Valentin Popov <VOPopov@luxoft.com>
2020-10-26 10:05:20 +0300

    Supervision demos

---
+++ src/main/scala/essentials/faulttolerance/SupervisionSpec.scala
@@ -0,0 +1,166 @@
+package essentials.faulttolerance
+
+import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume, Stop}
+import akka.actor.{Actor, ActorRef, ActorSystem, AllForOneStrategy, OneForOneStrategy, Props, SupervisorStrategy, Terminated}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import SupervisionSpec._
+
+  "A supervisor" should {
+    "resume its child in case of a minor fault" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love Akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! "Akka is awesome because I am learning to think in a whole new way"
+      child ! Report
+      expectMsg(3)
+    }
+
+    "restart its child in case of an empty sentence" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love Akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! ""
+      child ! Report
+      expectMsg(0)
+    }
+
+    "terminate its child in case of a major error" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! "akka is nice"
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+
+    "escalate an error when it doesn't know what to do" in {
+      val supervisor = system.actorOf(Props[Supervisor], "supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! 43
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+  }
+
+  "A kinder supervisor" should {
+    "not kill children in case it's restarted or escalates failures" in {
+      val supervisor = system.actorOf(Props[NoDeathOnRestartSupervisor], "supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "Akka is cool"
+      child ! Report
+      expectMsg(3)
+
+      child ! 45
+      child ! Report
+      expectMsg(0)
+    }
+  }
+
+  "An all-for-one supervisor" should {
+    "apply the all-for-one strategy" in {
+      val supervisor = system.actorOf(Props[AllForOneSupervisor], "allForOneSupervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      supervisor ! Props[FussyWordCounter]
+      val secondChild = expectMsgType[ActorRef]
+
+      secondChild ! "Testing supervision"
+      secondChild ! Report
+      expectMsg(2)
+
+      EventFilter[NullPointerException]() intercept {
+        child ! ""
+      }
+
+      Thread.sleep(500)
+
+      secondChild ! Report
+      expectMsg(0)
+    }
+  }
+}
+
+object SupervisionSpec {
+
+  class Supervisor extends Actor {
+
+    /**
+     * Applies the fault handling `Directive` (Resume, Restart, Stop) specified in the `Decider`
+     * to the child actor that failed, as opposed to [[akka.actor.AllForOneStrategy]] that applies
+     * it to all children.
+     * */
+    override val supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+      case _: NullPointerException => Restart // Discards the old Actor instance and replaces it with a new, then resumes message processing.
+      case _: IllegalArgumentException => Stop // Stops the Actor
+      case _: RuntimeException => Resume // Resumes message processing for the failed Actor
+      case _: Exception => Escalate // Escalates the failure to the supervisor of the supervisor,
+    }
+
+    override def receive: Receive = {
+      case props: Props =>
+        val childRef = context.actorOf(props)
+        sender() ! childRef
+    }
+  }
+
+  class NoDeathOnRestartSupervisor extends Supervisor {
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+      // empty - no restart child actors
+    }
+  }
+
+  class AllForOneSupervisor extends Supervisor {
+    override val supervisorStrategy = AllForOneStrategy() {
+      case _: NullPointerException => Restart
+      case _: IllegalArgumentException => Stop
+      case _: RuntimeException => Resume
+      case _: Exception => Escalate
+    }
+  }
+
+  case object Report
+  class FussyWordCounter extends Actor {
+    var words = 0
+
+    override def receive: Receive = {
+      case Report => sender() ! words
+      case "" => throw new NullPointerException("sentence is empty")
+      case sentence: String =>
+        if (sentence.length > 20) throw new RuntimeException("sentence is too big")
+        else if (!Character.isUpperCase(sentence(0))) throw new IllegalArgumentException("sentence must start with uppercase")
+        else words += sentence.split(" ").length
+      case _ => throw new Exception("can only receive strings")
+    }
+  }
+
+}
